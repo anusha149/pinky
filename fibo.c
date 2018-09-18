400c4ebd2941d57da3015ff1e806280556caca44
anusha <anushau015@gmail.com>
2018-09-18 11:20:37 +0530

    version 1

---
+++ fibo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int f1=0,f2=1,f3,i,n;
+    printf("enter the n value\n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+     {
+
+     if(n<=i)
+
+        f3=i;
+    else
+    {
+
+
+
+        f3=f1+f2;
+        f1=f2;
+        f2=f3;
+
+     }
+     }
+    printf("fibonacci series are %d\n",f3);
+    return 0;
+}
