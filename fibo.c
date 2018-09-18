bdd8e6d109263547367532b9c8eb346dd5c40104
anusha <anushau015@gmail.com>
2018-09-18 12:36:20 +0530

    version 1

--- fibo.c
+++ fibo.c
@@ -1,24 +1,71 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int fibo(int);
+
+
+
 int main()
+
 {
-    int f1=0,f2=1,f3,i,n;
-    printf("enter the n value\n");
-    scanf("%d",&n);
-    for(i=0;i<=n;i++)
-     {
 
-     if(n<=i)
+    int num;
 
-        f3=i;
+    int result;
+
+
+
+    printf("Enter the nth number in fibonacci series: ");
+
+    scanf("%d", &num);
+
+    if (num < 0)
+
+    {
+
+        printf("Fibonacci of negative number is not possible.\n");
+
+    }
+
     else
 
+    {
 
-        f3=f1+f2;
-        f1=f2;
-        f2=f3;
+        result = fibo(num);
 
-     }
-     }
-    printf("fibonacci series are %d\n",f3);
+        printf("The %d number in fibonacci series is %d\n", num, result);
+
+    }
+
     return 0;
+
+}
+
+int fibo(int num)
+
+{
+
+    if (num == 0)
+
+    {
+
+        return 0;
+
+    }
+
+    else if (num == 1)
+
+    {
+
+        return 1;
+
+    }
+
+    else
+
+    {
+
+        return(fibo(num - 1) + fibo(num - 2));
+
+    }
+
 }
